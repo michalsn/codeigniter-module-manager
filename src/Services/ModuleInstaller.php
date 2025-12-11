b1a7b5292cb45bb448ba119ca076c7f3999eb8f5
michalsn <michal@sniatala.pl>
2025-12-11 12:03:58 +0100

    refactor

--- src/Services/ModuleInstaller.php
+++ src/Services/ModuleInstaller.php
@@ -7,14 +7,16 @@ namespace Michalsn\CodeIgniterModuleManager\Services;
 use CodeIgniter\CodeIgniter;
 use Config\Services;
 use Exception;
-use Michalsn\CodeIgniterModuleManager\BaseModule;
 use Michalsn\CodeIgniterModuleManager\Config\ModuleManager as ModuleManagerConfig;
 use Michalsn\CodeIgniterModuleManager\Entities\Module;
 use Michalsn\CodeIgniterModuleManager\Exceptions\ModuleException;
 use Michalsn\CodeIgniterModuleManager\Models\ModuleModel;
+use Michalsn\CodeIgniterModuleManager\Traits\LoadsModuleInstance;
 
 class ModuleInstaller
 {
+    use LoadsModuleInstance;
+
     public function __construct(
         private readonly ModuleManagerConfig $config,
         private readonly ModuleModel $moduleModel,
@@ -300,39 +302,6 @@ class ModuleInstaller
 
             throw ModuleException::forMigrationRollbackGeneralFailure($e->getMessage());
         }
-    }
-
-    /**
-     * Load a module instance
-     * Returns the module's Module class instance
-     */
-    private function loadModuleInstance(Module $module): BaseModule
-    {
-        if (! $module->namespace) {
-            throw ModuleException::forNoNamespaceFound($module->folder_name);
-        }
-
-        $moduleFilePath = $this->config->folderPath . DIRECTORY_SEPARATOR . $module->folder_name . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Module.php';
-
-        if (! file_exists($moduleFilePath)) {
-            throw ModuleException::forModuleFileNotFound($module->folder_name);
-        }
-
-        require_once $moduleFilePath;
-
-        $className = $module->namespace . '\\Module';
-
-        if (! class_exists($className)) {
-            throw ModuleException::forModuleClassNotFound($className, $module->folder_name);
-        }
-
-        $instance = new $className();
-
-        if (! $instance instanceof BaseModule) {
-            throw ModuleException::forModuleMustExtendBaseModule($className);
-        }
-
-        return $instance;
     }
 
     private function callModuleMethod(Module $module, string $method, array $args = []): void
--- src/Services/RequirementsValidator.php
+++ src/Services/RequirementsValidator.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace Michalsn\CodeIgniterModuleManager\Services;
 
 use CodeIgniter\CodeIgniter;
-use Michalsn\CodeIgniterModuleManager\BaseModule;
 use Michalsn\CodeIgniterModuleManager\Config\ModuleManager as ModuleManagerConfig;
 use Michalsn\CodeIgniterModuleManager\Entities\Module;
 use Michalsn\CodeIgniterModuleManager\Exceptions\ModuleException;
+use Michalsn\CodeIgniterModuleManager\Traits\LoadsModuleInstance;
 
 class RequirementsValidator
 {
+    use LoadsModuleInstance;
+
     public function __construct(
         private readonly ModuleManagerConfig $config,
         private readonly ModuleRegistry $registry,
@@ -144,37 +146,5 @@ class RequirementsValidator
         } else {
             throw ModuleException::forInvalidDependencyVersionFormat($namespace);
         }
-    }
-
-    /**
-     * Load a module instance
-     */
-    private function loadModuleInstance(Module $module): BaseModule
-    {
-        if (! $module->namespace) {
-            throw ModuleException::forNoNamespaceFound($module->folder_name);
-        }
-
-        $moduleFilePath = $this->config->folderPath . DIRECTORY_SEPARATOR . $module->folder_name . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Module.php';
-
-        if (! file_exists($moduleFilePath)) {
-            throw ModuleException::forModuleFileNotFound($module->folder_name);
-        }
-
-        require_once $moduleFilePath;
-
-        $className = $module->namespace . '\\Module';
-
-        if (! class_exists($className)) {
-            throw ModuleException::forModuleClassNotFound($className, $module->folder_name);
-        }
-
-        $instance = new $className();
-
-        if (! $instance instanceof BaseModule) {
-            throw ModuleException::forModuleMustExtendBaseModule($className);
-        }
-
-        return $instance;
     }
 }
